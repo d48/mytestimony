0c6471bc009cd16099af35e839a24954af3af82f
Ryan Regalado <ryan@design48.net>
2012-11-08 22:30:01 -0800

    views and stylus files

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,16 @@
-
-/*
- * GET home page.
+/**
+ * Route object passed to app  
  */
+module.exports = {
+    index: function(req,res) {
+        res.render('index', {title: 'MyTestimony.com', page: 'home'})
+    }
+    , about: function(req,res) {
+        res.render('about', {title: 'MyTestimony.com - About', page: 'about'})
+    }
+     , contact: function(req,res) {
+        res.render('contact', {title: 'MyTestimony.com - Contact', page: 'contact'})
+    }
 
-exports.index = function(req, res){
-  res.render('index', 
-      { 
-          title: 'MyTestimony.com' 
-          , page: 'home'
-      }
-  )
-};
+}; 
 
-exports.about = function(req, res){
-  res.render('about', 
-      { 
-          title: 'MyTestimony.com - About' 
-          , page: 'about'
-      }
-  )
-};
-
-exports.contact = function(req, res){
-  res.render('about', { title: 'MyTestimony.com - Contact' })
-};
