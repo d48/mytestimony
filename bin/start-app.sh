c29b5a8105bccfa6ace37dd5c97a274ebeabee72
Ryan Regalado <ryan@design48.net>
2013-04-20 18:05:52 -0700

    add supervisor and typo in comment

--- bin/start-app.sh
+++ bin/start-app.sh
@@ -4,8 +4,8 @@ THIS_PID=$$
 # start mongodb
 mongod --dbpath ./data/db/ &
 
-# monitor node js app and atuo restart on directory/file changes
-supervisor -w app.js,routes,models -e 'js' app.js &
+# monitor node js app and auto restart on directory/file changes
+./node_modules/supervisor/lib/cli-wrapper.js -w app.js,routes,models -e 'js' app.js &
 
 echo "Run this to kill the script"
 echo "kill -TERM -$THIS_PID"
