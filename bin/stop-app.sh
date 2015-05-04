465dbf98fea3f956e95b0ce8f31bd7906d8acfa3
Ryan Regalado <ryan@design48.net>
2015-05-03 18:13:06 -0700

    fix stop scripts

--- bin/stop-app.sh
+++ bin/stop-app.sh
@@ -2,8 +2,18 @@
 
 # prints first column from output of jobs
 # JOBLINE=`ps ax | grep [bin/start]-app.sh | awk '{print $1}'`
-JOBLINE=`ps ax | grep "[npm] start" | awk '{print $1}'`
+# JOBLINE=`ps ax | grep "[npm] start" | awk '{print $1}'`
+JOBLINE=`ps ax | grep npm | awk '{print $1}' | head -n1`
+FOREVER=`ps ax | grep forever | awk '{print $1}' | head -n1`
 
 echo "running kill -TERM -${JOBLINE}"
 kill -TERM -${JOBLINE}
+
+echo "killing forever running kill ${FOREVER}"
+kill ${FOREVER}
+
+APPJS=`ps ax | grep app.js | awk '{print $1}' | head -n1`
+echo "killing app.js running kill ${APPJS}"
+kill ${APPJS}
+
 exit 1
