bead78834a186636fd783b0e1319325fcbdecb19
Ryan Regalado <ryan@design48.net>
2015-10-15 06:34:18 -0700

    update run script for heroku. set up procfile

--- bin/start-app.sh
+++ bin/start-app.sh
@@ -1,14 +1,7 @@
 #!/bin/bash 
 
-THIS_PID=$$
 # start mongodb
 mongod --dbpath ./data/db/ --smallfiles &
 
 # monitor node js app and auto restart on directory/file changes
-./node_modules/forever/bin/forever start app.js &
-
-echo "Run this to kill the script"
-echo "kill -TERM -$THIS_PID"
-echo ""
-
-echo "kill -TERM -$THIS_PID" | pbcopy
+./node_modules/forever/bin/forever start app.js
